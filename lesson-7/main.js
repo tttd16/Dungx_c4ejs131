ddde223fb393d23cbae18fb3ae1046d76fc78833
tttd16 <tiendung.work16@gmail.com>
2022-08-14 22:15:51 +0700

    bai tap lesson7

---
+++ lesson-7/main.js
@@ -0,0 +1,30 @@
+
+// let a = parseInt (prompt('nhập số giây'))
+
+
+// let b = Math.floor(a / 3600 % 24)
+// let c = Math.floor(a % 3600 / 60 )
+// let d = Math.floor(a % 3600 % 60)
+
+
+// document.write('số giờ:' , b)
+// document.write('số phút:' , c)
+// document.write('số giây:' , d)
+
+
+
+// Dùng charAt in ra ký tự y trong chuỗi sau
+let greeting = 'Hello. My name is John';
+document.write(greeting.charAt(8)+ '<br>' ); 
+
+
+
+// Dùng slice in ra chuỗi mới có giá trị is upon us từ chuỗi sau
+let str1 = 'The morning is upon us.'
+document.write(str1.slice(12,22) + '<br>');
+
+
+// DÙng concat để in ra chuỗi Hello World
+let str2 = 'Hello';
+let str3 = 'World';
+document.write(str2,' '.concat(str3) + '<br>');
