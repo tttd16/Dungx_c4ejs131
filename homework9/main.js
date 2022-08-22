2b33ae883096ae1f93d9190cbf8e3ae667ed7ce4
tttd16 <tiendung.work16@gmail.com>
2022-08-22 11:58:30 +0700

    homework9

---
+++ homework9/main.js
@@ -0,0 +1,50 @@
+//HW1
+//?
+// function checkAge(age) {
+//      age > 18? console.log(age) : confirm('Did parents allow you?')
+// }
+// checkAge(19)
+
+// OR ||
+// function checkAge(age) {
+//     (age > 18 && `${console.log(age)}`) || confirm('Did parents allow you?');
+//   }
+// checkAge(19)
+
+
+// HW 2 
+// function min(a,b) {
+//     a > b? console.log(a) : console.log(b)
+// }
+//  min(5,6)
+
+ //Hw 3
+
+//  let ask = (question, yes, no) => {
+//     if (confirm(question)) yes()
+//     else no();
+//  }
+
+//  ask(
+//     "Do you agree?",
+//     () => alert("You agreed.") ,
+//     () => alert("You canceled the execution.")
+//   );
+
+
+// HW 4
+
+// let login = prompt('Đăng nhập');
+// if (login == 'Admin') { 
+//     let password = prompt('Nhập mật khẩu');
+//     if (password == 'mindX') {
+//         alert('Chào mừng!')
+//     } else if (password == '' || password == null) {
+//         alert('Đã hủy')
+//     } else alert('Mật khẩu sai')
+// } else if (login == '' || login == null) {
+//     alert('Canceled')
+// } else alert('Tôi không biết bạn')
+
+
+
